e798e778bfbdae2f52bf2e26fd236a83bd1a0c9a
does-it-matter <zdravko_abramovic@protonmail.com>
2023-03-30 23:34:43 +0200

    composer.json file added, .htaccess file modified

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MEIN INDEX TITLE</title>
+    </head>
+    <body>
+        MEIN INDEX BODY
+    </body>
+</html>
+
+<?php
+require_once realpath("vendor/autoload.php");
+?>
